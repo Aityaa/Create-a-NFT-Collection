f156c95ac4ff86bd75ad1688d183f511edae952f
Aditya Singh <adityasingh98892111@gmail.com>
2024-05-28 09:33:24 +0530

    Update nftCollection.js

--- nftCollection.js
+++ nftCollection.js
@@ -8,10 +8,10 @@ Assessment Requirements
 4. For good measure, getTotalSupply() should return the number of NFTs you have created.
 */
 
-// Step 1: Create a variable to hold your NFTs
+
 let nfts = [];
 
-// Step 2: Function to mint NFTs
+
 function mintNFT(name, eyeColor, shirtType, bling) {
     // Create an NFT object with the provided metadata
     const nft = {
@@ -20,11 +20,11 @@ function mintNFT(name, eyeColor, shirtType, bling) {
         shirtType: shirtType,
         bling: bling
     };
-    // Store the NFT object in the nfts array
+   
     nfts.push(nft);
 }
 
-// Step 3: Function to list all NFTs
+
 function listNFTs() {
     // Loop through the nfts array and print metadata of each NFT
     for (let i = 0; i < nfts.length; i++) {
@@ -37,19 +37,19 @@ function listNFTs() {
     }
 }
 
-// Step 4: Function to get the total supply of NFTs
+
 function getTotalSupply() {
     // Return the total number of NFTs in the nfts array
     return nfts.length;
 }
 
-// Mint some NFTs
+
 mintNFT("CoolCat", "Blue", "T-Shirt", "Gold Chain");
 mintNFT("SmartDog", "Brown", "Sweater", "Silver Ring");
 mintNFT("ChillMonkey", "Green", "Hoodie", "Diamond Bracelet");
 
-// List all NFTs
+
 listNFTs();
 
-// Print the total supply of NFTs to the console
+
 console.log("Total Supply: " + getTotalSupply());
